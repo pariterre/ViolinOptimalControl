0833f651bdb0e4464a882bc1e74b96b6c066a448
ValentinAllard <valentin.allard@rocketmail.com>
2019-04-23 17:09:24 -0400

    Added example model to analyse

--- analyses/show_model.py
+++ analyses/show_model.py
@@ -19,26 +19,26 @@ import matplotlib.pyplot as plt
 nFrame = 100
 m = biorbd.s2mMusculoSkeletalModel("../models/Bras.pyoMod")
 # all_Q = np.ones((1,100)) * np.linspace(0, np.pi/2, nFrame)
+n_muscle = 18
 
 ##### State from the file
 
-q_init = np.zeros((m.nbQ(), 1))
-qdot_init = np.zeros((m.nbQdot(), 1))
+state_init = np.zeros((m.nbQ() + m.nbQdot()))
 
 ##### Controls from the file
 
-u = np.ones((m.nbQdot(), 1))*.5
+# u = np.ones(n_muscle)*.5
+u = np.ones(m.nbQddot())*5
 
 #### integration
 
 def dyn(t_int, X):
-    states_actual = biorbd.VecS2mMuscleStateActual(n_muscle)
-    for i in range(len(states_actual)):
-        states_actual[i] = biorbd.s2mMuscleStateActual(0, u[i])
+    # states_actual = biorbd.VecS2mMuscleStateActual(n_muscle)
+    # for i in range(len(states_actual)):
+    #     states_actual[i] = biorbd.s2mMuscleStateActual(0, u[i])
+    # Tau = biorbd.s2mMusculoSkeletalModel.muscularJointTorque(m, states_actual, X[:m.nbQ()], X[m.nbQ():])
 
-    Tau = biorbd.s2mMusculoSkeletalModel.muscularJointTorque(m, states_actual, X[:m.nbQ()], X[m.nbQ():])
-
-    QDDot = biorbd.s2mMusculoSkeletalModel.ForwardDynamics(m, X[:m.nbQ()], X[m.nbQ():], Tau).get_array()
+    QDDot = biorbd.s2mMusculoSkeletalModel.ForwardDynamics(m, X[:m.nbQ()], X[m.nbQ():], u).get_array()
     rsh = np.ndarray(m.nbQ() + m.nbQdot())
     for i in range(m.nbQ()):
         rsh[i] = X[m.nbQ()+i]
@@ -47,38 +47,39 @@ def dyn(t_int, X):
     return rsh
 
 
-LI = integrate.solve_ivp(dyn, (0, 1, X))
-Y = list()
-T = list
-for i in range(len(LI.t)-1):
-    Y.append(LI.y[q, :].tolist()[i])
-    T.append(LI.t.tolist()[i])
-
 #### Interpolation
-
-tck = interpolate.splrep(T, I, s=0)
-time = np.linspace(0, T[len(T)-1], nFrame)
-Y.append(interpolate.splev(time, tck, der=0))
+LI = integrate.solve_ivp(dyn, (0, .1), state_init)
+Y = list()
+Ydot = list()
+time = LI.t
+T = np.linspace(0, time[len(time)-1], nFrame)
+for i in range(m.nbQ()):
+    # Q
+    tck = interpolate.splrep(time, LI.y[i, :], s=0)
+    Y.append(interpolate.splev(T, tck, der=0))
+    # Qdot
+    tck = interpolate.splrep(time, LI.y[i+m.nbQ(), :], s=0)
+    Ydot.append(interpolate.splev(T, tck, der=0))
 
 ###### visualisation
 
-plt.figure(1)
-for i in range(m.nbQ()):
-    plt.subplot(m.nbQ(), 2, 1+(2*i))
-    plt.plot(all_Q[i])
-    plt.title("états %i" %i)
+# plt.figure(1)
+# for i in range(m.nbQ()):
+#     plt.subplot(m.nbQ(), 2, 1+(2*i))
+#     plt.plot(T, Y[i])
+#     plt.title("états %i" %i)
+#
+#     plt.subplot(m.nbQ(), 2, 2+(2*i))
+#     plt.plot(Ydot[i])
+#     plt.title("états dérivés %i" %i)
 
-    plt.subplot(m.nbQ(), 2, 2+(2*i))
-    plt.plot(all_Qdot[i])
-    plt.title("états dérivés %i" %i)
-
-plt.figure(2)
-for j in range(m.nbTau()):
-    plt.subplot(m.nbTau(), 1, i+1)
-    plt.plot(all_U[i])
-    plt.title("contrôles %i" %j)
-
-plt.show()
+# plt.figure(2)
+# for j in range(m.nbTau()):
+#     plt.subplot(m.nbTau(), 1, i+1)
+#     plt.plot(u[i])
+#     plt.title("contrôles %i" %j)
+#
+# plt.show()
 
 
 # Path to data
@@ -87,7 +88,7 @@ mark_tp = np.ndarray((3,  nTags, nFrame))
 q_tp = np.ndarray((m.nbQ()))
 for t in range(nFrame):
     for i in range(m.nbQ()):
-        q_tp[i] = Y[i][t]
+        q_tp[i] = Y[i][t]*0
     Q = biorbd.s2mGenCoord(q_tp)
     markers_tp = m.Tags(m, Q)
 
@@ -100,7 +101,7 @@ all_marks = Markers3d(mark_tp)
 vtkWindow = VtkWindow(background_color=(.5, .5, .5))
 
 # Add marker holders to the window
-vtkModelReal = VtkModel(vtkWindow, markers_color=(1, 0, 0), markers_size=.05, markers_opacity=1)
+vtkModelReal = VtkModel(vtkWindow, markers_color=(1, 0, 0), markers_size=.005, markers_opacity=1)
 
 # Animate all this
 i = 0
