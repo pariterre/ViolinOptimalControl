ecfa1c6b48e7ddeacff3d8fe49ab5feba404b3c2
ValentinAllard <valentin.allard@rocketmail.com>
2019-05-21 15:34:35 -0400

    Get viapoints

--- analyses/ConvertOsim2Biorbd.py
+++ analyses/ConvertOsim2Biorbd.py
@@ -62,7 +62,6 @@ def go_to(_root, _tag, _attrib=False, _attribvalue=''):
         _index = index_go_to(_root, _tag, _attrib, _attribvalue)
         return eval(retrieve_name(_root)+_index)
 
-
 class ConvertedFromOsim2Biorbd:
     def __init__(self, path, originfile):
 
@@ -76,13 +75,17 @@ class ConvertedFromOsim2Biorbd:
         self.file.write('File extracted from '+ self.originfile)
         self.file.write('\n')
 
-        self.file.close()
-
         def new_text(element):
             if type(element) == str:
                 return element
             else:
                 return element.text
+            
+        def print_credits():
+            return new_text(go_to(self.root, 'credits'))
+        
+        def print_publications():
+            return new_text(go_to(self.root, 'publications'))
 
         def body_list(self):
             L = []
@@ -112,10 +115,49 @@ class ConvertedFromOsim2Biorbd:
             return [new_text(go_to(go_to(self.root, 'Body', 'name', body), 'mass_center')),
                     new_text(go_to(go_to(self.root, 'Body', 'name', body), 'mass_center'))]
 
+        def muscle_list(self):
+            L = []
+            for muscle in self.data_origin.xpath(
+                    '/OpenSimDocument/Model/ForceSet/objects/Thelen2003Muscle'):
+                L.append(muscle.get("name"))
+            return L
+
+        def list_pathpoint_muscle(muscle):
+            #return list of viapoint for each muscle
+            viapoint = []
+            index_pathpoint = index_go_to(go_to(self.root, 'Thelen2003Muscle', 'name', muscle), 'PathPoint')
+            list_index = list(index_pathpoint)
+            tronc_list_index = list_index[:len(list_index)-2]
+            tronc_index = ''.join(tronc_list_index)
+            index_root = index_go_to(self.root, 'Thelen2003Muscle', 'name', muscle)
+            index_tronc_total = index_root+tronc_index
+            i = 0
+            while True:
+                try:
+                    child = eval('self.root'+index_tronc_total+str(i)+']')
+                    viapoint.append(child.get("name"))
+                    i += 1
+                except:
+                    break  
+            return viapoint
+
+        def get_body_pathpoint(pathpoint):
+            return go_to(go_to(self.root, 'PathPoint', 'name', pathpoint), 'body')
+        
+        # Credits
+        self.write('\n// CREDITS')
+        _credits = print_credits()
+        self.write('\n'+_credits+'\n')
+        
+         # Publications
+        self.write('\n// PUBLICATIONS\n')
+        _publications = print_publications()
+        self.write('\n'+_publications+'\n')
+
         # Segment definition
         self.write('\n// SEGMENT DEFINITION\n')
-        for body in body_list(self)[:]:
-            #segment data
+        for body in body_list(self):
+            # segment data
             parent = parent_body(body)
             rt_in_matrix = 1
             [r11, r12, r13, r14,
@@ -129,8 +171,8 @@ class ConvertedFromOsim2Biorbd:
             mass = mass_body(body)
             com = center_of_mass(body)
             mesh = extremities(body)
-            #writing data                  
-            self.write('\n// Informations about {} segment\n'
+            # writing data                  
+            self.write('\n// Information about {} segment\n'
                        '    // Segment\n'
                        '    segment {}\n'
                        '        parent {} \n'
@@ -145,8 +187,8 @@ class ConvertedFromOsim2Biorbd:
                                r21, r22, r23, r24,
                                r31, r32, r33, r34,
                                r41, r42, r43, r44))
-            self.write('        //translations xyz\n')
-            self.write('        //rotations xyz\n')
+            self.write('        //translations {}{}{}\n'.format('x','y','z'))
+            self.write('        //rotations {}{}{}\n'.format('x','y','z'))
             self.write('        mass {}\n'.format(mass))
             self.write('        inertia\n')
             self.write(
@@ -161,17 +203,30 @@ class ConvertedFromOsim2Biorbd:
             self.write('        mesh    {}\n'.format(mesh[1]))#center of mass
             self.write('    endsegment\n')
             
+        # Muscle definition
+        self.write('\n// MUSCLE DEFINIION\n')
+        for muscle in muscle_list(self):
+            #muscle data
+            viapoint = list_pathpoint_muscle(muscle)
+            bodies_viapoint = []
+            for pathpoint in viapoint:
+                bodies_viapoint.append(get_body_pathpoint(pathpoint))
+            body_start = bodies_viapoint[0]
+            body_end = bodies_viapoint[len(bodies_viapoint)-1]
+#            for bodies in bodies_viapoint[2:]:
+#                if bodies == parent_body(body_start):
+#                    body_start = bodies
+#                    print('*')
+#                elif parent_body(bodies) == body_end:
+#                    body_end = bodies
+#                    print('**')
+            print(body_start.text, body_end.text)
             
+            
+        self.file.close()
 
     def __getattr__(self, attr):
         print('Error : {} is not an attribute of this class'.format(attr))
-
-    # def __setattr__(self, name_attr, val_attr):
-    #     object.__setattr__(self, name_attr, val_attr)
-    #     self.enregistrer()
-
-    # def get_name(self):
-    #     return self.name
 
     def get_path(self):
         return self.path
@@ -198,7 +253,6 @@ class ConvertedFromOsim2Biorbd:
                 '/OpenSimDocument/Model/BodySet/objects/Body'):
             L.append(body.get("name"))
         return L
-
 
 
 def main():
