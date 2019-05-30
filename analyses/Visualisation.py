c94a45640898cd994079189c5576e935c0de8a50
ValentinAllard <valentin.allard@rocketmail.com>
2019-05-30 17:22:41 -0400

    Auto stash before merge of "master" and "origin/master"

--- analyses/Visualisation.py
+++ analyses/Visualisation.py
@@ -1,17 +1,5 @@
 from pyoviz.BiorbdViz import BiorbdViz
 
-import numpy as np
-
-import biorbd
-
-import scipy.integrate as integrate
-
-import scipy.interpolate as interpolate
-
-import matplotlib.pyplot as plt
-
-##### Load the model
-m = biorbd.s2mMusculoSkeletalModel("/home/lim/Programmation/ViolinOptimalControl/models/testconversion0.biomod")
-bio = BiorbdViz(loaded_model = m, show_meshes=False)
-
-bio.exec() # so visualisation window doesnt shut off
+# Load the model
+bio = BiorbdViz(model_path="../models/Bras.bioMod", show_meshes=False)
+bio.exec()  # So visualisation window doesnt shut off
